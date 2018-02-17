b16fea76a0b4ddfc1bbf6d5d6e7323a6e83ff4b4
Catarina Canto <catarinaccanto@gmail.com>
2018-02-17 14:34:49 +0000

    enemyFactory

--- src/org/bootcamp/academiadecodigo/hexallents/newgame/factory/EnemyFactory.java
+++ src/org/bootcamp/academiadecodigo/hexallents/newgame/factory/EnemyFactory.java
@@ -1,4 +1,26 @@
 package org.bootcamp.academiadecodigo.hexallents.newgame.factory;
 
+import org.bootcamp.academiadecodigo.hexallents.newgame.enemy.Enemy;
+import org.bootcamp.academiadecodigo.hexallents.newgame.enemy.EnemyType;
+
 public class EnemyFactory {
+
+    public static Enemy getNewEnemy() {
+
+        int random = (int) (Math.random() * EnemyType.values().length);
+        EnemyType enemyType = EnemyType.values()[random];
+
+        switch (enemyType) {
+            case ENEMY_1:
+                return new Enemy(enemyType);
+
+            case ENEMY_2:
+                return new Enemy(enemyType);
+
+            case ENEMY_3:
+                return new Enemy(enemyType);
+        }
+
+        return null;
+    }
 }
