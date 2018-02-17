d5c0b7fbadb21801e4b70ea43c7b89dece365bb4
Catarina Canto <catarinaccanto@gmail.com>
2018-02-17 14:22:48 +0000

    character factory

--- src/org/bootcamp/academiadecodigo/hexallents/newgame/factory/CharacterFactory.java
+++ src/org/bootcamp/academiadecodigo/hexallents/newgame/factory/CharacterFactory.java
@@ -1,4 +1,26 @@
 package org.bootcamp.academiadecodigo.hexallents.newgame.factory;
 
+import org.bootcamp.academiadecodigo.hexallents.newgame.character.CharacterType;
+import org.bootcamp.academiadecodigo.hexallents.newgame.character.Character;
+
+
 public class CharacterFactory {
+
+    public static Character getNewCharacter(){
+
+        int random = (int) (Math.random() * CharacterType.values().length);
+        CharacterType characterType = CharacterType.values()[random];
+
+        switch (characterType) {
+            case CHARACTER_1:
+                return new Character(characterType);
+
+            case CHARACTER_2:
+                return new Character(characterType);
+
+            case CHARACTER_3:
+                return new Character(characterType);
+        }
+
+    }
 }
